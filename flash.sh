1669143bdfb0940059a9e5181b9792d4c77a02d1
Kees Jan Koster <kjkoster@kjkoster.org>
2025-07-25 09:35:59 +0200

    Flash much faster still.

--- flash.sh
+++ flash.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 ELF_BINARY=${1}
+
+# Extract the binary from the ELF file in the format that the board expects
 rust-objcopy -O binary ${ELF_BINARY} ${ELF_BINARY}.bin
-stm32flash -w ${ELF_BINARY}.bin -v -g 0x0 -b 115200 ${STM_SERIAL_PORT}
+
+# Write the binary to the board via the serial port. If you experience any
+# issues flashing, try lowering the baud rate from 921600 to 115200.
+stm32flash -w ${ELF_BINARY}.bin -v -g 0x0 -b 921600 ${STM_SERIAL_PORT}
